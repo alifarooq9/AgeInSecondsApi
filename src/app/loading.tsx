1437fedfa462c8796940486e109ca8f5047037df
Ali Muhammad <alimuhammadfc5@gmail.com>
2023-05-06 14:51:05 +0500

    loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,12 @@
+import { Loader2 } from "lucide-react";
 import { FC } from "react";
 
 const Loading: FC = () => {
-	return <div>Loading...</div>;
+	return (
+		<div className="w-full mt-36 flex justify-center items-center">
+			<Loader2 className="h-9 w-9 animate-spin" />
+		</div>
+	);
 };
 
 export default Loading;
-    
